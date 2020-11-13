6bb085a55a69998863d4fffca79bab3c5b0ab335
YogiPristiawan <yogistanada252@gmail.com>
2020-11-13 11:55:50 +0700

    cuman edit spasi (biar rapi bos)

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -36,7 +36,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'titiktemu',
+		'database' => '',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
